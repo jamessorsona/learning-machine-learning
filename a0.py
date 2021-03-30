3b4727867c870982eabddad082a394b75b12f4fb
James Carlo Sorsona <34911646+jamessorsona@users.noreply.github.com>
2021-03-30 21:22:30 +0800

    added notebook, figured out how to setup an anaconda environment in vs code

---
+++ a0.py
@@ -0,0 +1,34 @@
+# Author: James Carlo Sorsona
+
+""" Machine Learning Tutorial
+Website: https://machinelearningmastery.com/machine-learning-in-python-step-by-step/ """
+
+# 2.1 Load libraries
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+# 2.2 Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+# print(dataset)
+
+
+# 3.1 Dimenstions of the dataset
+print(dataset.shape)
+
+# 3.2 Peeking at the data
